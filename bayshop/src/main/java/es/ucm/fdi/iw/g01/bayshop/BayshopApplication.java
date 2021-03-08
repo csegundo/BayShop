bd1fd032131a7507c19225cdc0453766470c905b
csegundo <carlossn20165@gmail.com>
2021-03-08 22:53:08 +0100

    First commit

---
+++ bayshop/src/main/java/es/ucm/fdi/iw/g01/bayshop/BayshopApplication.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.iw.g01.bayshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayshopApplication.class, args);
+	}
+
+}
---
+++ bayshop/src/test/java/es/ucm/fdi/iw/g01/bayshop/BayshopApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.iw.g01.bayshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
