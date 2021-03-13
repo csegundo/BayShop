e161b9b4530d2ef6b1f12aed93cb5151a7145cf1
Daniel <daalca01@ucm.es>
2021-03-13 02:07:13 +0100

    vista Producto aceptable. Clase Producto en Controller para poder pasar a la vista.

--- bayshop/src/main/java/es/ucm/fdi/iw/g01/bayshop/controller/RootController.java
+++ bayshop/src/main/java/es/ucm/fdi/iw/g01/bayshop/controller/RootController.java
@@ -1,5 +1,7 @@
 package es.ucm.fdi.iw.g01.bayshop.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -13,6 +15,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class RootController {
     private static Logger logger = LogManager.getLogger(RootController.class);
 
+    public static class Producto {
+        
+        private String nombre;
+        private String marca;
+        private String color;
+        private String talla;
+        private Double precio;
+        private String estado;
+        private String descripcion;
+        private Date fecha;
+
+
+        public Producto(String nombre, String marca, String color, String talla, Double precio, String estado, String descripcion) {
+            this.nombre = nombre;
+            this.marca = marca;
+            this.color = color;
+            this.talla = talla;
+            this.precio = precio;
+            this.estado = estado;
+            this.descripcion = descripcion;
+            //this.fecha = fecha;
+        }
+
+        public String getNombre() { return nombre; }
+        public String getMarca() { return marca; }
+        public String getColor() {return color;}
+        public String getTalla() {return talla;}
+        public Double getPrecio() {return precio;}
+        public String getEstado() {return estado;}
+        public String getDescripcion() {return descripcion;}
+        public Date getFecha() {return fecha;}
+    }
+    
     @GetMapping("/") 
     public String index(HttpSession session, Model model, @RequestParam(required = false) Integer entero){
         return "index";
@@ -39,7 +74,10 @@ public class RootController {
     }
 
     @GetMapping("/producto/id")
-    public String product_id() {
+    public String product_id(Model model) {
+        model.addAttribute("user", "comprador");
+        Producto p = new Producto("Camiseta chula", "Adidas", "Rojo", "XL", 23.45, "Nueva", "La vendo porque me queda grande");
+        model.addAttribute("p", p);
         return "producto";
     }
 
