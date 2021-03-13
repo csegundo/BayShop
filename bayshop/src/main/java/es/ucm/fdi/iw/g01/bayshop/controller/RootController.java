5e375c4f23913a08926d19b7f65cad74f0e660f4
Daniel <daalca01@ucm.es>
2021-03-13 19:11:51 +0100

    vistas index y producto. controller para probar las vistas.

--- bayshop/src/main/java/es/ucm/fdi/iw/g01/bayshop/controller/RootController.java
+++ bayshop/src/main/java/es/ucm/fdi/iw/g01/bayshop/controller/RootController.java
@@ -25,10 +25,12 @@ public class RootController {
         private String estado;
         private String descripcion;
         private Date fecha;
+        private String vendedor;
 
 
-        public Producto(String nombre, String marca, String color, String talla, Double precio, String estado, String descripcion) {
+        public Producto(String nombre,String marca, String color, String talla, Double precio, String estado, String descripcion) {
             this.nombre = nombre;
+            this.vendedor = "Rick";
             this.marca = marca;
             this.color = color;
             this.talla = talla;
@@ -40,6 +42,7 @@ public class RootController {
 
         public String getNombre() { return nombre; }
         public String getMarca() { return marca; }
+        public String getVendedor() {return vendedor;}
         public String getColor() {return color;}
         public String getTalla() {return talla;}
         public Double getPrecio() {return precio;}
@@ -48,8 +51,21 @@ public class RootController {
         public Date getFecha() {return fecha;}
     }
     
+    Producto prod [] = new Producto []{
+        new Producto("Camiseta chula", "Adidas", "Rojo", "XL", 23.45, "Nueva", "La vendo porque me queda grande"),
+        new Producto("Camiseta fea", "Reebok", "Rojo", "XL", 30.25, "Nueva", "La vendo porque me queda grande"),
+        new Producto("Camiseta fina", "Adidas", "Rojo", "XS", 18.25, "Nueva", "La vendo porque me queda pequeña"),
+        new Producto("Albornoz", "Adidas", "Rojo", "XL", 30.25, "Nuevo", "La vendo porque me queda grande"),
+        new Producto("Abrigo", "Nike", "Rojo", "XL", 30.25, "Nueva", "La vendo porque me queda grande"),
+        new Producto("Chandal", "Adidas", "Rojo", "XL", 30.25, "Nueva", "La vendo porque me queda grande"),
+        new Producto("Gorra", "P&B", "Rojo", "XL", 30.25, "Nueva", "La vendo porque me queda pequeña")
+    };
+
+
     @GetMapping("/") 
     public String index(HttpSession session, Model model, @RequestParam(required = false) Integer entero){
+
+        model.addAttribute("prod", prod);
         return "index";
     }
 
@@ -75,9 +91,13 @@ public class RootController {
 
     @GetMapping("/producto/id")
     public String product_id(Model model) {
+        
         model.addAttribute("user", "comprador");
+
         Producto p = new Producto("Camiseta chula", "Adidas", "Rojo", "XL", 23.45, "Nueva", "La vendo porque me queda grande");
         model.addAttribute("p", p);
+        model.addAttribute("prod", prod);
+
         return "producto";
     }
 
