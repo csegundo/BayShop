76ebad684fbbb4895c71bc01d29c251d2e5160f5
csegundo <carlossn20165@gmail.com>
2021-03-17 21:22:56 +0100

    Updated leeme.html y arreglado SpringSecurity config

--- bayshop/src/main/java/es/ucm/fdi/iw/g01/bayshop/SecurityConfig.java
+++ bayshop/src/main/java/es/ucm/fdi/iw/g01/bayshop/SecurityConfig.java
@@ -1,12 +1,14 @@
 package es.ucm.fdi.iw.g01.bayshop;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
+    @Override
 	protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeRequests()
@@ -16,5 +18,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
             .loginPage("/login")
             .permitAll();
+	}
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // allows access to h2 console iff running under debug mode
+        web.ignoring().antMatchers("/h2/**");
 	}
 }
