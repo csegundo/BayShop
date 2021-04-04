efeeb38b97cfbbb7ddb51d7f87cfb00ae753f395
Daniel <daalca01@ucm.es>
2021-04-04 22:32:59 +0200

    Modificaciones para hacer login

--- bayshop/src/main/resources/import.sql
+++ bayshop/src/main/resources/import.sql
@@ -5,19 +5,26 @@
 --
 
 -- Usuario de ejemplo con username = b y contraseña = aa  
-INSERT INTO user(id,enabled,username,password,roles,first_name,last_name) VALUES (
-	1, 1, 'a', 
+INSERT INTO user(enabled,username,password,roles,first_name,last_name, email, baypoints) VALUES (
+	1, 'a', 
 	'{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u',
-	'USER,ADMIN',
-	'Abundio', 'Ejémplez'
+	'ADMIN',
+	'Abundio', 'Ejémplez', 'abundio@gmail.com', 1
 );
 
 -- Otro usuario de ejemplo con username = b y contraseña = aa  
-INSERT INTO user(id,enabled,username,password,roles,first_name,last_name) VALUES (
-	2, 1, 'b', 
+INSERT INTO user(enabled,username,password,roles,first_name,last_name, email, baypoints) VALUES (
+	1, 'b', 
 	'{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u',
 	'USER',
-	'Berta', 'Muéstrez'
+	'Berta', 'Muéstrez', 'berta@gmail.com', 2
+);
+
+INSERT INTO user (enabled,username,password,roles,first_name,last_name, email, baypoints) VALUES (
+	1,'ElPerico', 
+	'{bcrypt}$2a$10$xLFtBIXGtYvAbRqM95JhcOaG23fHRpDoZIJrsF2cCff9xEHTTdK1u',
+	'USER',
+	'Pedro', 'Garcia', 'pericoLoko@hotmail.com', 3
 );
 
 -- Unos pocos auto-mensajes de prueba
@@ -26,3 +33,8 @@ INSERT INTO MESSAGE VALUES(2,NULL,'2020-03-23 10:48:15.149000','probando 2',1,1)
 INSERT INTO MESSAGE VALUES(3,NULL,'2020-03-23 10:48:18.005000','probando 3',1,1);
 INSERT INTO MESSAGE VALUES(4,NULL,'2020-03-23 10:48:20.971000','probando 4',1,1);
 INSERT INTO MESSAGE VALUES(5,NULL,'2020-03-23 10:48:22.926000','probando 5',1,1);
+
+
+--Productos de ejemplo
+INSERT INTO PRODUCT (name, description, creation_date, price, size, brand, status, categories, enabled)
+VALUES ( 'Camiseta chula', 'camiseta roja, me queda grande', '2012-09-17 18:47:52.69', 21.3, 'XL', 'NIKE', 0, 'SHIRT', '1');
